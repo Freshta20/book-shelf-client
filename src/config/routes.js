5316c4e6a2ffc60825c1b136bf536704fae1f848
freshta <freshta.bashari@gmail.com>
2020-06-07 22:22:27 -0700

    added the routes for login and logout

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Register from '../pages/Register';
 import { Switch, Route } from 'react-router-dom';
+import Login from '../pages/Login';
 
 const Routes = (props) => (
   <Switch>
     <Route exact path='/register' component={ Register } />
+    <Route path="/login" render={ (routeComponentProps) => {
+      return <Login 
+               { ...routeComponentProps }
+               currentUser={ props.currentUser }
+               storeUser={ props.storeUser }
+             />
+    }} />
+
   </Switch>
 )
 
