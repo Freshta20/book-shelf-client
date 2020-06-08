935c9ce7f8dbefc7771b8920a25200472dee825b
freshta <freshta.bashari@gmail.com>
2020-06-07 23:18:47 -0700

    completed the user login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,69 @@
-import React, { Component } from 'react'
-import UserModel from '../models/user'
+import React, { useState } from 'react'
+import UserModel from '../models/UserModel'
 
-class Login extends Component {
+const Login = (props) =>  {
+  const[userData, setUserData] = useState({
+    email: '',
+    password: '',
+  })
+
+  // handle form input change
+const handleChange = (event) => {
+  let newState = Object.assign({}, userData);
+    newState[event.target.name] = event.target.value;
+    setUserData(newState);
+}
   
+// handle form submission
+const handleSubmit = (event) => {
+  event.preventDefault();
+  // 
+  UserModel.login(userData)
+  .then(data => {
+    console.log(data);
+    // if(data.status === 200){
+    //   props.StoreUser(data.data)
+    // }
+    // reset the form inputs
+    setUserData(
+      {
+      email: '',
+      password: '',
+
+      }
+    )
+  })
+}
+
+  return (
+    <div>
+      Login Page
+      <form onSubmit={handleSubmit}>
+      <div>
+          <label htmlFor="email">Email</label>
+          <input 
+          onChange={handleChange}
+          type="text" 
+          id="email"
+          name="email" 
+          value={userData.email}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input 
+          onChange={handleChange}
+          type="password" 
+          id="password"
+          name="password" 
+          value={userData.password}
+          />
+        </div>
+        <button type="submit">Login in</button>
+      </form>
+    </div>
+  )
 }
 
 
