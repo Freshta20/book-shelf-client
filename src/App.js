4b70f4382c64d89ee893b6278f4064a8fb116ffa
freshta <freshta.bashari@gmail.com>
2020-06-07 23:18:03 -0700

    added the logout part

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import Routes from './config/routes';
-
+import Navbar from './components/Navbar';
+import UserModel from './models/UserModel';
 import './App.css';
 
-function App() {
+function App(props) {
   const [currentUser, setCurrentUser] = useState(localStorage.getItem('uid'));
   const storeUser = (userId) => {
     setCurrentUser(userId)
     localStorage.setItem('uid', userId)
   }
+
+  const logout = (event) => {
+    event.preventDefault()
+
+    localStorage.removeItem('uid')
+    UserModel.logout()
+      .then(res => {
+        console.log(res)
+        setCurrentUser(null)
+        props.history.push('/login')
+      })
+  } 
   return (
     <div className="App">
+     <Navbar 
+     currentUser={ currentUser } 
+     logout={ logout }
+     />
+
       hello
       <Routes
       currentUser={ currentUser }
