2070f25a6107ed7aa5ae9e7b90db571d4e955ce4
freshta <freshta.bashari@gmail.com>
2020-06-09 11:38:45 -0700

    added the model for category

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -1,13 +1,25 @@
-import axios from 'axios';
 
-const categoryEndpoint = 'http://localhost:4000/api/v1/categories';
-const cookieAxios = axios.create({
-  withCredentials: true
-});
+const url = 'http://localhost:4000/api/v1/categories';
+
 export default class CategoryModel {
-  // index rout
-  static index = () => {
-    const request = cookieAxios.get(categoryEndpoint);
-    return request;
+  static all = () => {
+    return fetch(`${url}/index`).then(res => res.json())
   }
 }
+
+
+
+
+
+
+
+// const cookieAxios = axios.create({
+//   withCredentials: true
+// });
+// export default class CategoryModel {
+//   // index rout
+//   static index = () => {
+//     const request = cookieAxios.get(categoryEndpoint);
+//     return request;
+//   }
+// }
