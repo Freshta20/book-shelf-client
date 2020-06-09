21857cab0d2c147c1218809dc0117aa462c7d8a1
freshta <freshta.bashari@gmail.com>
2020-06-09 14:34:44 -0700

    fixed the fetch for categorylist component

--- src/models/CategoryModel.js
+++ src/models/CategoryModel.js
@@ -2,15 +2,35 @@
 const url = 'http://localhost:4000/api/v1/categories';
 
 export default class CategoryModel {
+// GET all categories
   static all = () => {
-    return fetch(`${url}/index`).then(res => res.json())
+    console.log(url);
+    return fetch(`${url}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: 'include'
+    })
+    .then(res => res.json())
   }
+
+  
 }
 
 
 
 
-
+// static login (credentials) {
+//    return fetch(`${REACT_APP_API}/login`, {
+//      method: "POST",
+//      headers: {
+//        "Content-Type": "application/json"
+//      },
+//      credentials: 'include',
+//      body: JSON.stringify(credentials)
+//    }).then(res => res.json())
+//   }
 
 
 // const cookieAxios = axios.create({
