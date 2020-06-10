77c10eee788eec072827364e9dae6bcd77e2c814
freshta <freshta.bashari@gmail.com>
2020-06-09 21:56:38 -0700

    added delete button for deleting each category

--- src/pages/CategoryList.js
+++ src/pages/CategoryList.js
@@ -12,6 +12,7 @@ class CategoryList extends Component {
     this.fetchData()
   }
 
+ 
   fetchData = () => {
     CategoryModel.all()
     .then(data =>{
@@ -19,13 +20,23 @@ class CategoryList extends Component {
       this.setState({ categories: data.categories })})
     .catch(err => console.log(err))
   }
+  handleDelete = async (id) => {
+    try{ CategoryModel.delete(id)
+  }catch(err){
+    console.log(err)
+  }
+}
 
   render(){
     let categoryList = this.state.categories.map((category, index) => {
     return( 
+      <>
       <Link key={index} to={`/categories/${ category._id }`}>
           <CategoryCard { ...category}/>
       </Link>
+              <button  onClick={()=>this.handleDelete(category._id)}>Delete</button>
+              </>
+
     )
     })
     return(
