00a280119f3c0b1cfbc5dc27a3569695b12f90f3
freshta <freshta.bashari@gmail.com>
2020-06-10 11:04:10 -0700

    made bootstrap working on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,34 +9,34 @@ const Navbar = (props) => {
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-    <>
+   
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
         { props.currentUser ? 
           <>
           <li className="nav-item active">
-            <a className="nav-link" href="{% url 'home' %}">Logout</a>
+          <a className="nav-link" href="/logout" onClick={ props.logout }>Log Out</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="{% url 'add_movies' %}">Category List</a>
+          <Link className="nav-link" to={'/categories'}>CategoriesList</Link>
           </li>
          
           <li className="nav-item">
-            <a className="nav-link disabled" href="{% url 'login_user' %}">New Category</a>
+          <Link className="nav-link disabled" to={'/categories/new'}>New Category</Link>
           </li>
             </>
               :
             <>
           <li className="nav-item">
-            <a className="nav-link disabled" href="{% url 'register' %}">Register</a>
+          <Link className="nav-link disabled" to={'/register'}>Register</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="{% url 'register' %}">Login</a>
+          <Link className="nav-link disabled" to={'/login'}>Login</Link>
           </li>
-          <>
+          </>
         }
         </ul>
-        </div>
+       </div>
      </nav>
      </>
   );
