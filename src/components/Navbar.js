dce76597a7ddfcefe818dae56f4d7ad26c04a80b
freshta <freshta.bashari@gmail.com>
2020-06-10 11:58:54 -0700

    bring changes to the color of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom'
 const Navbar = (props) => {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark text-white">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light  sticky-top">
+      <div className="container">
+        
       <a className="navbar-brand" href="#">BOOK-SHELF</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon  justify-content-end"></span>
       </button>
    
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
+        <ul className="navbar-nav mr-auto w-100 justify-content-end">
         { props.currentUser ? 
           <>
           <li className="nav-item active">
@@ -22,20 +24,21 @@ const Navbar = (props) => {
           </li>
          
           <li className="nav-item">
-          <Link className="nav-link disabled" to={'/categories/new'}>New Category</Link>
+          <Link className="nav-link " to={'/categories/new'}>New Category</Link>
           </li>
             </>
               :
             <>
           <li className="nav-item">
-          <Link className="nav-link disabled" to={'/register'}>Register</Link>
+          <Link className="nav-link " to={'/register'}>Register</Link>
           </li>
           <li className="nav-item">
-          <Link className="nav-link disabled" to={'/login'}>Login</Link>
+          <Link className="nav-link " to={'/login'}>Login</Link>
           </li>
           </>
         }
         </ul>
+       </div>
        </div>
      </nav>
      </>
