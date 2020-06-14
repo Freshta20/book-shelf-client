8755a2b4ac005d0962574f0275aca7fde54f12d6
freshta <freshta.bashari@gmail.com>
2020-06-14 14:06:26 -0700

    added the delete book method on model

---
+++ src/models/BookModel.js
@@ -0,0 +1,30 @@
+
+const book_url = 'http://localhost:4000/api/v1/books';
+
+export default class BookModel {
+
+  // POST new book
+  static create = (categoryId, bookData) => {
+    return fetch(`${book_url}/${categoryId}/books/new`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: 'include',
+      body: JSON.stringify(bookData)
+    })
+    .then(res => res.json())
+  }
+
+ // DELETE a category
+ static delete = (categoryId, bookId) => {
+  return fetch(`${book_url}/${categoryId}/books/${bookId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: 'include'
+  })
+  .then(res => res.json())
+}
+}
